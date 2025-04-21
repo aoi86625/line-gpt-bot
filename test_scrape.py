23bcfe97ea50cc2cd1836c7edc356a4c387b564d
aoi86625 <aoi86625@gmail.com>
2025-04-21 16:38:40 +0900

    Update test_scrape.py

--- test_scrape.py
+++ test_scrape.py
@@ -6,23 +6,19 @@ on:
 jobs:
   run-test:
     runs-on: ubuntu-latest
-
     steps:
-      - name: Checkout repo
-        uses: actions/checkout@v3
+      - uses: actions/checkout@v3
 
       - name: Set up Python
         uses: actions/setup-python@v4
         with:
-          python-version: "3.10"
+          python-version: 3.10
 
       - name: Install Poetry
-        run: |
-          python -m pip install --upgrade pip
-          pip install poetry
+        run: pip install poetry
 
-      - name: Install dependencies
-        run: poetry install --no-root  # ← ここが重要！
+      - name: Install dependencies (no-root)
+        run: poetry install --no-root
 
       - name: Run scraping test
         run: poetry run python test_scrape.py
