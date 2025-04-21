ef8e6d12b31691096b708b07312c7e328ee0b191
aoi86625 <aoi86625@gmail.com>
2025-04-21 16:53:11 +0900

    Update test_scrape.py

--- test_scrape.py
+++ test_scrape.py
@@ -4,21 +4,13 @@ on:
   workflow_dispatch:
 
 jobs:
-  run-test:
+  scrape:
     runs-on: ubuntu-latest
+
     steps:
       - uses: actions/checkout@v3
 
       - name: Set up Python
         uses: actions/setup-python@v4
         with:
-          python-version: 3.10
-
-      - name: Install Poetry
-        run: pip install poetry
-
-      - name: Install dependencies (no-root)
-        run: poetry install --no-root
-
-      - name: Run scraping test
-        run: poetry run python test_scrape.py
+          python-version:
